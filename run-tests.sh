6d64b3613e7fb3157e8373825957ad8ed069cb6e
Ian W. Scott <scottianw@gmail.com>
2025-10-18 22:54:38 -0400

    fix(testing): Harmonize test runner with other KCWorks projects

--- run-tests.sh
+++ run-tests.sh
@@ -16,32 +16,32 @@ set -o nounset
 
 # Always bring down docker services
 function cleanup() {
-    eval "$(uv run docker-services-cli down --env)"
+  eval "$(uv run docker-services-cli down --env)"
 }
 
 # Check if any docker-compose projects are running
 function check_docker_compose_running() {
-    echo "Checking for running docker-compose projects..."
+  echo "Checking for running docker-compose projects..."
 
-    # Get list of running containers that might be from docker-compose
-    running_containers=$(docker ps --format "table {{.Names}}\t{{.Image}}" | grep -E "(postgres|redis|opensearch|rabbitmq|elasticsearch)" || true)
+  # Get list of running containers that might be from docker-compose
+  running_containers=$(docker ps --format "table {{.Names}}\t{{.Image}}" | grep -E "(postgres|redis|opensearch|rabbitmq|elasticsearch)" || true)
 
-    if [ -n "$running_containers" ]; then
-        echo "Warning: Found potentially conflicting containers running:"
-        echo "$running_containers"
-        echo ""
-        echo "This might cause port conflicts with docker-services-cli."
-        echo "Consider stopping any running docker-compose projects before continuing."
-        echo ""
-        read -p "Do you want to continue anyway? (y/N): " -n 1 -r
-        echo
-        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-            echo "Aborting. Please stop conflicting containers and try again."
-            exit 1
-        fi
-    else
-        echo "No conflicting containers detected."
+  if [ -n "$running_containers" ]; then
+    echo "Warning: Found potentially conflicting containers running:"
+    echo "$running_containers"
+    echo ""
+    echo "This might cause port conflicts with docker-services-cli."
+    echo "Consider stopping any running docker-compose projects before continuing."
+    echo ""
+    read -p "Do you want to continue anyway? (y/N): " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      echo "Aborting. Please stop conflicting containers and try again."
+      exit 1
     fi
+  else
+    echo "No conflicting containers detected."
+  fi
 }
 
 # Check for arguments
@@ -50,48 +50,52 @@ keep_services=0
 skip_translations=0
 pytest_args=()
 for arg in $@; do
-	# from the CLI args, filter out some known values and forward the rest to "pytest"
-	# note: we don't use "getopts" here b/c of some limitations (e.g. long options),
-	#       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
-	case ${arg} in
-		-K|--keep-services)
-			keep_services=1
-			;;
-		-S|--skip-translations)
-			skip_translations=1
-			;;
-		*)
-			pytest_args+=( ${arg} )
-			;;
-	esac
+  # from the CLI args, filter out some known values and forward the rest to "pytest"
+  # note: we don't use "getopts" here b/c of some limitations (e.g. long options),
+  #       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
+  case ${arg} in
+  -K | --keep-services)
+    keep_services=1
+    ;;
+  -S | --skip-translations)
+    skip_translations=1
+    ;;
+  *)
+    pytest_args+=(${arg})
+    ;;
+  esac
 done
 
 if [[ ${keep_services} -eq 0 ]]; then
-	trap cleanup EXIT
+  trap cleanup EXIT
 fi
 
 # Extract and compile translations from python files
 if [[ ${skip_translations} -eq 0 ]]; then
-	echo "Extracting translations from python files"
-	uv run invenio-cli translations extract
-	echo "Updating translations"
-	uv run invenio-cli translations update
-	echo "Compiling translations"
-	uv run invenio-cli translations compile
+  echo "Extracting translations from python files"
+  uv run invenio-cli translations extract
+  echo "Updating translations"
+  uv run invenio-cli translations update
+  echo "Compiling translations"
+  uv run invenio-cli translations compile
 else
-	echo "Skipping translations compilation"
+  echo "Skipping translations compilation"
 fi
+
+# Build the documentation
+echo "Building the documentation"
+uv run sphinx-build -b html docs/source/ docs/build/
 
 # Check for running docker-compose projects before starting services
 check_docker_compose_running
 
 # Check if tests_stats_dashboard/.env exists and set env_file_arg accordingly
 if [ -f "tests_stats_dashboard/.env" ]; then
-	env_file_arg="--env-file tests_stats_dashboard/.env"
-	echo "Using tests_stats_dashboard/.env file for environment variables"
+  env_file_arg="--env-file tests_stats_dashboard/.env"
+  echo "Using tests_stats_dashboard/.env file for environment variables"
 else
-	env_file_arg=""
-	echo "No tests_stats_dashboard/.env file found, using default environment"
+  env_file_arg=""
+  echo "No tests_stats_dashboard/.env file found, using default environment"
 fi
 
 # Start the services and get their environment variables
@@ -106,15 +110,14 @@ unset INVENIO_SQLALCHEMY_DATABASE_URI
 echo "Running mypy on the invenio_stats_dashboard directory"
 uv run mypy --config-file pyproject.toml invenio_stats_dashboard/
 
-
 # Note: expansion of pytest_args looks like below to not cause an unbound
 # variable error when 1) "nounset" and 2) the array is empty.
 if [ ${#pytest_args[@]} -eq 0 ]; then
-	echo "Running pytest"
-	uv run ${env_file_arg} python -m pytest -vv -s --disable-warnings
+  echo "Running pytest"
+  uv run ${env_file_arg} python -m pytest -vv -s --disable-warnings
 else
-	echo "Running pytest with additional arguments"
-	uv run ${env_file_arg} python -m pytest ${pytest_args[@]} -s --disable-warnings
+  echo "Running pytest with additional arguments"
+  uv run ${env_file_arg} python -m pytest ${pytest_args[@]} -s --disable-warnings
 fi
 
 tests_exit_code=$?
